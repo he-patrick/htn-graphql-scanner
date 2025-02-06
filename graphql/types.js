c53001bc1638d49e1526a49ef9b654a4888ed261
Patrick He <he.patrick2005@gmail.com>
2025-02-06 00:30:26 -0500

    Create user and scan graphql types

---
+++ graphql/types.js
@@ -0,0 +1,34 @@
+import { GraphQLObjectType, GraphQLString, GraphQLList, GraphQLID } from "graphql";
+import User from "../models/User.js";
+import Scan from "../models/Scan.js";
+
+const UserType = new GraphQLObjectType({
+  name: "User",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    badge_code: { type: GraphQLString },
+    updated_at: { type: GraphQLString },
+    scans: {
+      type: new GraphQLList(ScanType),
+      resolve: (user) => Scan.findAll({ where: { userId: user.id } }),
+    },
+  }),
+});
+
+const ScanType = new GraphQLObjectType({
+  name: "Scan",
+  fields: () => ({
+    id: { type: GraphQLID },
+    activity_name: { type: GraphQLString },
+    activity_category: { type: GraphQLString },
+    scanned_at: { type: GraphQLString },
+    user: {
+      type: UserType,
+      resolve: (scan) => User.findByPk(scan.userId),
+    },
+  }),
+});
+
+export { UserType, ScanType }
