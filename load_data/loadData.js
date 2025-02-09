7dd72eeedce9e88531e53a590047cd34738b7afc
Patrick He <he.patrick2005@gmail.com>
2025-02-08 19:51:08 -0500

    Fix userId reference

--- load_data/loadData.js
+++ load_data/loadData.js
@@ -19,7 +19,7 @@ async function loadData() {
 
     for (const scan of userData.scans) {
       await Scan.create({
-        user_id: user.userId,
+        userId: user.userId,
         activity_name: scan.activity_name,
         activity_category: scan.activity_category,
         scanned_at: scan.scanned_at
