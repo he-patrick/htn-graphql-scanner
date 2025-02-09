7a8a093937b1e8b419528079bb25743ba7d41ef4
Patrick He <he.patrick2005@gmail.com>
2025-02-08 19:52:03 -0500

    Remove unused imports

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import sequelize from "./db.js";
 import schema from "./graphql/schema.js";
-import Scan from "./models/Scan.js";
-import User from "./models/User.js";
 
 const port = process.env.PORT || 3000;
 const app = express();
