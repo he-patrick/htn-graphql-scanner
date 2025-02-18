497822cd584414419ea8c0a8f3deaa516a532953
Patrick He <he.patrick2005@gmail.com>
2025-02-17 20:45:02 -0500

    Update db setup for AWS RDS

--- db.js
+++ db.js
@@ -1,10 +1,19 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-const databaseUrl = process.env.DATABASE_URL || "postgres://postgres:password@localhost:5432/htn_backend_challenge";
+dotenv.config();
+
+const databaseUrl = process.env.DATABASE_URL || "postgres://postgres:password@htn-backend-challenge.c72ys44ic8jb.us-east-2.rds.amazonaws.com:5432/htn_backend_challenge";
 
 const sequelize = new Sequelize(databaseUrl, {
   dialect: "postgres",
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 export default sequelize;
