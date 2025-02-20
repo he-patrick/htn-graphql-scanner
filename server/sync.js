2e53cfe7d74bf5bb76b77f524abe483fc963124e
Patrick He <he.patrick2005@gmail.com>
2025-02-20 15:54:17 -0500

    Update AWS RDS sync function

--- server/sync.js
+++ server/sync.js
@@ -2,13 +2,14 @@ import sequelize from "./db.js";
 import User from "./src/models/User.js";
 import Scan from "./src/models/Scan.js";
 import Mealtime from "./src/models/Mealtime.js";
+import Karaoke from "./src/models/Karaoke.js";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const syncDatabase = async () => {
   try {
-    await sequelize.sync({ force: true });
+    await sequelize.sync({ alter: true });
     console.log("Database synchronized!");
   } catch (error) {
     console.error("Error synchronizing database:", error);
